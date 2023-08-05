00906e7e386b11210e2c860a32cb4c9fa9a6683e
James Tennant <jtennant@ucsc.edu>
2023-08-05 15:47:40 -0700

    dummy screens

--- src/components/NavStack.tsx
+++ src/components/NavStack.tsx
@@ -1,22 +1,19 @@
-import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
+import Home from './screens/Home';
+import Auth from './screens/Auth';
+import Chat from './screens/Chat';
 
 const Stack = createNativeStackNavigator();
 
 export default function NavStack() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Navigator initialRouteName={false ? 'Home' : 'Auth'}>
+        <Stack.Screen name="Auth" component={Auth} />
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Chat" component={Chat} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ src/components/screens/Auth.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function Auth() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Auth Screen</Text>
+    </View>
+  );
+}
