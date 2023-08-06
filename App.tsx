eb5a60d00c331f63a3df1af07ea3743f4a68f467
James Tennant <jtennant@ucsc.edu>
2023-08-05 18:55:47 -0700

    style app container

--- App.tsx
+++ App.tsx
@@ -1,10 +1,25 @@
+import { View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+
+import { colors } from './src/theme/theme';
 import NavStack from './src/components/NavStack';
 import ViewModel from './src/model/ViewModel';
 
 export default function App() {
   return (
-    <ViewModel>
-      <NavStack />
-    </ViewModel>
+    <View
+      style={{
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        backgroundColor: colors.grey200,
+      }}>
+      <View style={{ flex: 1, maxWidth: 900, backgroundColor: colors.white }}>
+        <ViewModel>
+          <NavStack />
+        </ViewModel>
+      </View>
+      <StatusBar style="dark" />
+    </View>
   );
 }
--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -3,6 +3,11 @@ import { StyleSheet } from 'react-native';
 export const colors = {
   blue: '#08f',
   white: '#fff',
+  grey100: '#f5f5f5',
+  grey200: '#ddd',
+  grey300: '#ccc',
+  grey400: '#aaa',
+  grey500: '#777',
 };
 
 export const styles = StyleSheet.create({
