86d13bcfb1c620bcd474f697cfd1e21575399609
James Tennant <jtennant@ucsc.edu>
2023-08-07 00:34:29 -0700

    render realtime posts

--- src/components/post/List.tsx
+++ src/components/post/List.tsx
@@ -1,15 +1,52 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { FlatList, Text } from 'react-native';
+import { supabase } from '../../lib/supabase';
+
+interface Post {
+  id: string;
+  poster: string;
+  content: string;
+  date: string;
+}
 
 export default function PostList() {
-  const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      let { data } = await supabase.from('posts').select();
+      setPosts(data!);
+    };
+    fetchPosts();
+
+    const postListener = supabase
+      .channel('public:posts')
+      .on(
+        'postgres_changes',
+        { event: '*', schema: 'public', table: 'posts' },
+        (payload) => {
+          setPosts((prevPosts) => {
+            if (payload.eventType === 'INSERT') {
+              return [payload.new as Post, ...prevPosts];
+            } else {
+              return prevPosts.filter((post) => post.id !== payload.old.id);
+            }
+          });
+        }
+      )
+      .subscribe();
+
+    return () => {
+      supabase.removeChannel(postListener);
+    };
+  }, []);
 
   return (
     <FlatList
       data={posts}
       style={{ height: 0 }}
       keyboardDismissMode="on-drag"
-      renderItem={({ item }) => <Text>item</Text>}
+      renderItem={({ item }) => <Text>{JSON.stringify(item)}</Text>}
     />
   );
 }
