f96337f203b26567759ea6e44d7a2fcbbdacb923
James Tennant <jtennant@ucsc.edu>
2023-08-07 02:16:22 -0700

    select hydrated posts

--- src/components/post/List.tsx
+++ src/components/post/List.tsx
@@ -3,11 +3,17 @@ import { FlatList, Text } from 'react-native';
 import { supabase } from '../../lib/supabase';
 import Loading from '../common/Loading';
 
+export interface User {
+  id: string;
+  full_name: string;
+  avatar_url: string;
+}
+
 interface Post {
   id: string;
-  poster: string;
   content: string;
   date: string;
+  profiles: User;
 }
 
 export default function PostList() {
@@ -16,9 +22,14 @@ export default function PostList() {
 
   useEffect(() => {
     const fetchPosts = async () => {
-      let { data, error } = await supabase.from('posts').select();
+      let { data, error } = await supabase.from('posts').select(`
+        id,
+        content,
+        date,
+        profiles ( id, full_name, avatar_url )
+      `);
       if (error) alert(error.message);
-      else setPosts(data!);
+      else setPosts(data as any);
       setLoading(false);
     };
     fetchPosts();
@@ -28,14 +39,28 @@ export default function PostList() {
       .on(
         'postgres_changes',
         { event: '*', schema: 'public', table: 'posts' },
-        (payload) => {
-          setPosts((prevPosts) => {
-            if (payload.eventType === 'INSERT') {
-              return [payload.new as Post, ...prevPosts];
-            } else {
+        async (payload) => {
+          if (payload.eventType === 'INSERT') {
+            let { data, error } = await supabase
+              .from('posts')
+              .select(
+                `
+                  id,
+                  content,
+                  date,
+                  profiles ( id, full_name, avatar_url )
+                `
+              )
+              .eq('id', payload.new.id);
+            if (error) alert(error.message);
+            setPosts((prevPosts) => {
+              return [data![0] as any, ...prevPosts];
+            });
+          } else {
+            setPosts((prevPosts) => {
               return prevPosts.filter((post) => post.id !== payload.old.id);
-            }
-          });
+            });
+          }
         }
       )
       .subscribe();
@@ -52,7 +77,7 @@ export default function PostList() {
       data={posts}
       style={{ height: 0 }}
       keyboardDismissMode="on-drag"
-      renderItem={({ item }) => <Text>{JSON.stringify(item)}</Text>}
+      renderItem={({ item }) => <Text>{JSON.stringify(item, null, 2)}</Text>}
     />
   );
 }
