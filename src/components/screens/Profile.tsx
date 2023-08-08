befbd3dd6a7fa682d4d0884cdcbc2b4446c7d97e
James Tennant <jtennant@ucsc.edu>
2023-08-07 23:19:48 -0700

    fetch hydrated profile

--- src/components/screens/Profile.tsx
+++ src/components/screens/Profile.tsx
@@ -1,13 +1,38 @@
+import { useEffect, useState } from 'react';
 import { Text, View } from 'react-native';
 
 import { useViewModel } from '../../model/ViewModel';
+import { supabase } from '../../lib/supabase';
+
+interface Profile {
+  id: string;
+  full_name: string;
+  avatar_url: string;
+  location: string;
+  website: string;
+  bio: string;
+}
 
 export default function Profile() {
   const { selectedUser } = useViewModel();
+  const [profile, setProfile] = useState<null | Profile>(null);
+
+  useEffect(() => {
+    const getProfile = async () => {
+      let { data, error } = await supabase
+        .from('profiles')
+        .select(`location, website, bio`)
+        .eq('id', selectedUser.id)
+        .single();
+      if (error) alert(error.message);
+      else setProfile({ ...selectedUser, ...data });
+    };
+    getProfile();
+  }, []);
 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>{selectedUser.full_name}'s Profile</Text>
+      <Text>{JSON.stringify(profile)}</Text>
     </View>
   );
 }
