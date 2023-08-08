630a4b3aec0070eba131284a55379c001ca979be
James Tennant <jtennant@ucsc.edu>
2023-08-07 20:08:55 -0700

    fetch chats unfiltered

--- src/components/screens/Chats.tsx
+++ src/components/screens/Chats.tsx
@@ -1,9 +1,40 @@
-import { Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { FlatList, Text, View } from 'react-native';
+
+import { User } from '../../model/ViewModel';
+import { supabase } from '../../lib/supabase';
 
 export default function Chats() {
+  const [chats, setChats] = useState<User[]>([]);
+
+  useEffect(() => {
+    const fetchChats = async () => {
+      const { data, error } = await supabase
+        .from('profiles')
+        .select('id, full_name, avatar_url');
+      if (error) alert(error.message);
+      else setChats(data);
+    };
+    fetchChats();
+  });
+
+  if (chats.length === 0)
+    return (
+      <View
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center',
+          padding: 20,
+        }}>
+        <Text>You haven't sent or received any messages yet</Text>
+      </View>
+    );
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Chats</Text>
-    </View>
+    <FlatList
+      data={chats}
+      renderItem={({ item }) => <Text>{JSON.stringify(item, null, 2)}</Text>}
+    />
   );
 }
