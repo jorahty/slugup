6a0b479e3c804732caf24913dee50172dc762f9d
James Tennant <jtennant@ucsc.edu>
2023-08-09 23:35:13 -0700

    on deactivate sign out

--- src/components/screens/Deactivate.tsx
+++ src/components/screens/Deactivate.tsx
@@ -1,13 +1,15 @@
 import { Text, View } from 'react-native';
+
 import { styles } from '../../theme/theme';
 import Button from '../common/Button';
+import { signOut } from '../../repo/auth';
 
 export default function Deactivate() {
   return (
     <View style={{ gap: 20, padding: 40, paddingTop: 50 }}>
       <Text style={styles.headline}>Deactivate your account?</Text>
       <View style={{ maxWidth: 150 }}>
-        <Button variant="danger" title="Deactivate" />
+        <Button variant="danger" title="Deactivate" onPress={signOut} />
       </View>
       <Text>This action cannot be undone</Text>
     </View>
--- src/components/screens/Settings.tsx
+++ src/components/screens/Settings.tsx
@@ -1,15 +1,15 @@
 import { Text, TouchableOpacity, View } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { FontAwesome5 } from '@expo/vector-icons';
 
 import { styles } from '../../theme/theme';
-import { FontAwesome5 } from '@expo/vector-icons';
-import { supabase } from '../../lib/supabase';
+import { signOut } from '../../repo/auth';
 
 export default function Settings() {
   const { navigate } = useNavigation<any>();
   return (
     <View style={{ gap: 40, padding: 40, paddingTop: 50 }}>
-      <TouchableOpacity onPress={() => supabase.auth.signOut()}>
+      <TouchableOpacity onPress={signOut}>
         <View style={{ flexDirection: 'row', gap: 10 }}>
           <FontAwesome5 name="heart-broken" size={24} color="black" />
           <Text style={styles.headline}>Sign out</Text>
