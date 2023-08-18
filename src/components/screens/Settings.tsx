0f64b10ebf1bbbbfd6e669398f885d3edde25201
James Tennant <jtennant@ucsc.edu>
2023-08-18 16:53:39 -0700

    fix settings icons

--- src/components/screens/Settings.tsx
+++ src/components/screens/Settings.tsx
@@ -11,13 +11,13 @@ export default function Settings() {
     <View style={{ gap: 40, padding: 40, paddingTop: 50 }}>
       <TouchableOpacity onPress={signOut}>
         <View style={{ flexDirection: 'row', gap: 10 }}>
-          <FontAwesome5 name="heart-broken" size={24} color="black" />
+          <FontAwesome5 name="sign-out-alt" size={24} color="black" />
           <Text style={styles.headline}>Sign out</Text>
         </View>
       </TouchableOpacity>
       <TouchableOpacity onPress={() => navigate('Deactivate')}>
         <View style={{ flexDirection: 'row', gap: 10 }}>
-          <FontAwesome5 name="sign-out-alt" size={24} color="black" />
+          <FontAwesome5 name="heart-broken" size={24} color="black" />
           <Text style={styles.headline}>Deactivate your account</Text>
         </View>
       </TouchableOpacity>
